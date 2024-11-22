0be480ecddbb4a5be35b9cec6d3e4dd630abe593
Shivangam Soni <shivangamsoni99@gmail.com>
2024-11-22 17:36:47 +0530

    Add: Utils for Image Access

---
+++ src/utils.tsx
@@ -0,0 +1,7 @@
+import apple from "/assets/images/apple.svg";
+import search from "/assets/images/search.svg";
+import cartBag from "/assets/images/cartBag.svg";
+
+export const appleImg = apple;
+export const searchImg = search;
+export const cartBagImg = cartBag;
